5025f03b35a1c2bb44b0c230ccaf834b042b6608
eddie naff <eddienaff@eddienaff.com>
2019-12-11 15:12:15 -0800

    Add custom link component

---
+++ modules/site-layout/my-link.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+
+interface IStyledLinkProps {
+  shade?: string | undefined;
+}
+
+interface MyLinkProps {
+  href: string;
+  name: string;
+  shade?: string | undefined;
+  prefetch?: boolean;
+}
+
+const StyledLink = styled.a<IStyledLinkProps>`
+  text-decoration: none;
+  color: ${(props: any) =>
+    props.shade === "dark" ? props.theme.colors.text : "white"};
+  :hover {
+    color: crimson;
+  }
+`;
+
+const MyLink: React.FunctionComponent<MyLinkProps> = ({
+  href,
+  name,
+  prefetch,
+  shade
+  // theme
+}) => (
+  <Link href={href} prefetch={prefetch ? true : false} passHref>
+    <StyledLink shade={shade ? shade : undefined}>{name}</StyledLink>
+  </Link>
+);
+
+export default MyLink;
