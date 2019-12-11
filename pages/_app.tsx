7676ad4aa67e51aa8a9689c1e234b4f4ac861bf8
eddie naff <eddienaff@eddienaff.com>
2019-12-11 15:12:37 -0800

    Add theme to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,23 @@
 import App, { Container } from "next/app";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "styled-components";
 import { withApollo } from "../lib/apollo";
+import { GlobalStyles } from "../modules/site-layout/global-styles";
+import { theme } from "../modules/site-layout/theme";
 
 class MyApp extends App<any> {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
     return (
-      <Container>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyles />
+          <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </Container>
+      </ThemeProvider>
     );
   }
 }
