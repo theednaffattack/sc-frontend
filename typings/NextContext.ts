efb8e46744bf6cb44aff9f4754e6561c9d172f5e
eddie naff <eddienaff@eddienaff.com>
2019-12-12 15:29:32 -0800

    Add NextContext typings

---
+++ typings/NextContext.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, NormalizedCacheObject } from "apollo-boost";
+import { NextPageContext } from "next";
+
+export interface NextContext extends NextPageContext {
+  apolloClient?: ApolloClient<NormalizedCacheObject>;
+  referer?: string;
+}
