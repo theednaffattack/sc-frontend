19462a9b15383e508db613fb7e46eaa4bb6bb776
eddie naff <eddienaff@eddienaff.com>
2019-12-13 17:53:37 -0800

    Extract logout function

---
+++ lib/logout.ts
@@ -0,0 +1,11 @@
+import { isBrowser } from "./isBrowser";
+
+export const logout = (): void => {
+  let now = new Date();
+
+  if (isBrowser) {
+    // To trigger the event listener we save some random data into the `logout` key
+    window.localStorage.setItem("logout", now.toISOString());
+    console.log("window?", window.localStorage.getItem("logout"));
+  }
+};
