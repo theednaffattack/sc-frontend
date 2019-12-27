c2ca972e2bec41a86f008c5dd2ac6d1de4d2d09c
eddie naff <eddienaff@eddienaff.com>
2019-12-26 17:43:17 -0800

    Add prop to opt out of hover events

--- modules/site-layout/my-link.tsx
+++ modules/site-layout/my-link.tsx
@@ -1,35 +1,52 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
+import { space, SpaceProps } from "styled-system";
 
 interface IStyledLinkProps {
   shade?: string | undefined;
+  hover?: boolean;
 }
 
-interface MyLinkProps {
+interface MyLinkProps extends SpaceProps {
   href: string;
   name: string;
   shade?: string | undefined;
+  hover?: boolean;
 }
 
-const StyledLink = styled.a<IStyledLinkProps>`
+const StyledLink = styled.a<IStyledLinkProps & SpaceProps>`
+  ${space}
   text-decoration: none;
+  border-bottom: 2px transparent solid;
+  padding-bottom: 8px;
+
   color: ${(props: any) =>
     props.shade === "dark" ? props.theme.colors.text : "white"};
+
   :hover {
-    color: crimson;
+    color: ${props => (props.hover === false ? "green" : "crimson")};
+    border-bottom: ${props =>
+      props.hover === false ? "2px transparent solid" : "2px limegreen solid"};
   }
 `;
 
 const MyLink: React.FunctionComponent<MyLinkProps> = ({
+  children,
   href,
-  name,
-  shade
-  // theme
-}) => (
-  <Link href={href} passHref>
-    <StyledLink shade={shade ? shade : undefined}>{name}</StyledLink>
-  </Link>
-);
+  shade,
+  hover,
+  ...props
+}) => {
+  // const initialState = false;
+  // const [hovering, setHovering] = useState<boolean>(initialState);
+  return (
+    <Link href={href} passHref>
+      <StyledLink {...props} hover={hover} shade={shade ? shade : undefined}>
+        {children}
+      </StyledLink>
+    </Link>
+  );
+};
 
 export default MyLink;
