eb406aa40166d9b4eeb46eab502caa522c4e8c65
eddie naff <eddienaff@eddienaff.com>
2019-12-26 17:43:42 -0800

    Add state to app from hoc and layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,14 +8,32 @@ import { theme } from "../modules/site-layout/theme";
 
 class MyApp extends App<any> {
   render() {
-    const { Component, pageProps, apolloClient } = this.props;
+    const {
+      Component,
+      pageProps,
+      apolloClient,
+      hocLogin,
+      hocLogout,
+      hocLoginState,
+      token
+    } = this.props;
     const getLayout = Component.getLayout || ((page: any) => page);
     const title = Component.title || "Fake test title";
+    console.log("VIEW THIS.PROPS (/pages/_app.tsx)", this.props);
     return (
       <ApolloProvider client={apolloClient}>
         <GlobalStyles />
         <ThemeProvider theme={theme}>
-          {getLayout(<Component title={title} {...pageProps} />)}
+          {getLayout(
+            <Component
+              title={title}
+              token={token}
+              hocLogin={hocLogin}
+              hocLogout={hocLogout}
+              hocLoginState={hocLoginState}
+              {...pageProps}
+            />
+          )}
         </ThemeProvider>
       </ApolloProvider>
     );
