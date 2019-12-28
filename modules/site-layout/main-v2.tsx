0a21867a41f55eed94791984db5046385d88f9ea
eddie naff <eddienaff@eddienaff.com>
2019-12-27 17:34:50 -0800

    Drill login and logout functions down for now

--- modules/site-layout/main-v2.tsx
+++ modules/site-layout/main-v2.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import Head from "next/head";
-import { Flex as FlexBase } from "rebass/styled-components";
-import { minHeight, borders } from "styled-system";
-import styled from "styled-components";
+// import { minHeight, borders } from "styled-system";
+// import styled from "styled-components";
 
+import { Flex } from "../primitives/styled-rebass";
 import Header from "./header";
 
 // interface MenuState {
@@ -14,55 +14,66 @@ import Header from "./header";
 interface LayoutProps {
   title?: string;
   hocLoginState: boolean;
+  hocLogin: () => void;
+  hocLogout: () => void;
+  token?: string;
 }
 
-const Flex = styled(FlexBase)`
-  ${minHeight}
-  ${borders}
-`;
+// const Flex = styled(FlexBase)`
+//   ${minHeight}
+//   ${borders}
+// `;
 
 export const breakWidths = [1, 1, 1, "960px"];
 
 const Layout: React.FunctionComponent<LayoutProps> = ({
   children,
   hocLoginState,
-  title = "This is the default title"
+  hocLogout,
+  title = "This is the default title",
+  token
 }) => {
-  const initialState = true;
-  // @ts-ignore
-  const [authState, setAuthState] = useState(initialState);
-  let elementWithAuthSetter;
-  if (React.isValidElement(children)) {
-    elementWithAuthSetter = React.cloneElement(children, {
-      authState,
-      setAuthState
-    });
-  }
+  // const initialState = true;
+  // // @ts-ignore
+  // const [authState, setAuthState] = useState(initialState);
+  // let elementWithAuthSetter;
+  // if (React.isValidElement(children)) {
+  //   elementWithAuthSetter = React.cloneElement(children, {
+  //     authState,
+  //     setAuthState
+  //   });
+  // }
   return (
-    <Flex m={[0]} minHeight="100vh" flexDirection="column" width={[1]}>
+    <Flex m={[0]} minHeight="100vh" flexDirection="column" width={1}>
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Flex
-        justifyContent="center"
-        alignItems="center"
-        sx={{ border: "2px white dashed" }}
-      >
-        <Header hocLoginState={hocLoginState} />
+      <Flex justifyContent="center" alignItems="center">
+        <Header
+          hocLogout={hocLogout}
+          token={token}
+          hocLoginState={hocLoginState}
+        />
       </Flex>
-      <Flex flexDirection="column">{elementWithAuthSetter}</Flex>
+      <Flex width={1} flexDirection="column">
+        {children}
+      </Flex>
     </Flex>
   );
 };
 
 export const getLayout = (page: any) => {
-  console.log("VIEW GETLAYOUT PAGE (/modules/site-layout/main-v2.tsx)", page);
-
   return (
-    <Layout hocLoginState={page.props.hocLoginState} title={page.props.title}>
+    <Layout
+      token={page.props.token}
+      hocLoginState={page.props.hocLoginState}
+      hocLogin={page.props.hocLogin}
+      hocLogout={page.props.hocLogout}
+      title={page.props.title}
+    >
       {page}
     </Layout>
   );
