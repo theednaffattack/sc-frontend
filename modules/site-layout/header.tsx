43499aca55fab7baae4c5d40ab861a6c231c9d02
eddie naff <eddienaff@eddienaff.com>
2020-01-05 11:27:01 -0800

    Add state to header

--- modules/site-layout/header.tsx
+++ modules/site-layout/header.tsx
@@ -1,25 +1,29 @@
 import React from "react";
-// import { minHeight, borders } from "styled-system";
 import styled from "styled-components";
-
-import { Flex, Image } from "../primitives/styled-rebass";
-import MyLink from "./my-link";
 import {
   width,
   WidthProps,
   borders,
   BordersProps,
   minHeight,
-  MinHeightProps
+  MinHeightProps,
+  SpaceProps,
+  space
 } from "styled-system";
+
+import { Flex, Image } from "../primitives/styled-rebass";
+import MyLink from "./my-link";
+import { useRouter } from "next/router";
 
 interface HeaderProps {
   hocLoginState: boolean;
   hocLogout: () => void;
+  referer: string;
+  syncLogout: () => void;
   token?: string;
 }
 
-const Nav = styled.nav<WidthProps & BordersProps & MinHeightProps>`
+const Nav = styled.nav<WidthProps & BordersProps & MinHeightProps & SpaceProps>`
   color: #ccc;
   display: flex;
   align-items: center;
@@ -28,27 +32,34 @@ const Nav = styled.nav<WidthProps & BordersProps & MinHeightProps>`
   ${minHeight}
   ${width}
   ${borders}
+  ${space}
 `;
 
 const RenderLoginLink = () => {
   return (
-    <>
-      <MyLink shade="dark" href="/login" name="login">
-        Login
-      </MyLink>
-    </>
+    <MyLink shade="dark" href="/login" name="login">
+      Login
+    </MyLink>
   );
 };
 
 interface RenderLogoutProps {
   hocLogout?: () => void;
+  syncLogout?: () => void;
 }
 
 const RenderLogout: React.FunctionComponent<RenderLogoutProps> = ({
-  hocLogout
+  hocLogout,
+  syncLogout
 }) => {
   return (
-    <MyLink hocLogout={hocLogout} shade="dark" href="/logout" name="/logout">
+    <MyLink
+      syncLogout={syncLogout}
+      hocLogout={hocLogout}
+      shade="dark"
+      href="/logout"
+      name="/logout"
+    >
       Logout
     </MyLink>
   );
@@ -71,53 +82,212 @@ const RenderAvatarLink = () => {
   );
 };
 
-// const RenderAvatarPlaceholder = () => {
-//   return (
-//     <svg
-//       viewBox="0 0 50 50"
-//       style={{
-//         minWidth: "48px",
-//         minHeight: "48px",
-//         borderBottom: "2px transparent solid",
-//         paddingBottom: "8px"
-//       }}
-//     >
-//       <circle cx="25" cy="25" r="25" fill="pink"></circle>
-//     </svg>
-//   );
-// };
+interface LabelProps {
+  label: string;
+}
 
-// interface RenderForgotPasswordProps {}
+function Logger(props: LabelProps) {
+  console.log(`${props.label} rendered`);
+  return null; // what is returned here is irrelevant...
+}
 
-// const RenderForgotPassword: React.FunctionComponent<RenderForgotPasswordProps> = () => {
-//   return (
-//     <MyLink shade="dark" href="/forgot-password" name="forgot-password">
-//       Forgot Password
-//     </MyLink>
-//   );
-// };
+// function Label(props: LabelProps) {
+//   return <span>{props.label}</span>;
+// }
 
-// @ts-ignore
 const Header: React.FunctionComponent<HeaderProps> = ({
   hocLoginState,
   hocLogout,
+  referer,
+  syncLogout,
   token
 }) => {
-  const breakWidths = [1, 1, 1, "690px"];
+  // @ts-ignore
+  const [count, setCount] = React.useState(0);
+  // const increment = () => setCount(c => c + 1);
 
-  const isAuthenticated = token || hocLoginState === true;
+  const breakWidths = [1, 1, "690px", "690px"];
+  let { query } = useRouter();
 
-  return (
-    <Flex flexDirection="column">
-      <Nav minHeight="58px" width={breakWidths}>
-        <MyLink shade="dark" href="/" name="home">
-          Home
-        </MyLink>{" "}
-        {!isAuthenticated ? <RenderLoginLink /> : <RenderAvatarLink />}
-        {isAuthenticated ? <RenderLogout hocLogout={hocLogout} /> : ""}
-      </Nav>
-    </Flex>
-  );
+  let getQuery;
+
+  let isAnArray =
+    query && query.message instanceof Array ? query.message[0] : undefined;
+  let isAString =
+    query && typeof query.message === "string" ? query.message : undefined;
+
+  getQuery = isAString ? isAString : isAnArray ? isAnArray : "unknown";
+
+  let makeAuthenticatedFalseMatches = [
+    "Not authenticated",
+    "logout",
+    "You have successfully logged out."
+  ];
+  let shouldntBeAuthed =
+    makeAuthenticatedFalseMatches.indexOf(getQuery, 0) !== -1;
+
+  if (shouldntBeAuthed === true && hocLoginState === true) {
+    hocLogout();
+    console.log("hoc login state", hocLoginState);
+  }
+  let isAuthenticated;
+  // // if they're server rendered
+  // isAuthenticated =
+  //   token && hocLoginState === false && shouldntBeAuthed === false;
+
+  // specifically if they just logged in
+  isAuthenticated = hocLoginState || !!token;
+
+  // if (shouldntBeAuthed === true) {
+  //   isAuthenticated = false;
+  // }
+
+  type RenderDecisionEnum = "okay to render" | "do not render";
+
+  type LoginStatusEnum =
+    | "i just logged in"
+    | "i logged in another way"
+    | "i'm not authenticated"
+    | "i am authenticated";
+
+  let loginStatus: LoginStatusEnum = token
+    ? "i am authenticated"
+    : !token &&
+      hocLoginState === true &&
+      referer.includes("login") &&
+      shouldntBeAuthed === false
+    ? "i just logged in"
+    : hocLoginState === true
+    ? "i logged in another way"
+    : "i'm not authenticated";
+
+  let renderDecision: RenderDecisionEnum =
+    shouldntBeAuthed === true || isAuthenticated === false
+      ? "do not render" // hocLogin state is false || query message is "Not authenticated"
+      : loginStatus === "i just logged in" ||
+        loginStatus === "i am authenticated"
+      ? "okay to render"
+      : loginStatus === "i logged in another way"
+      ? "okay to render"
+      : "do not render";
+  console.log("WHAT IS RENDER DECISION (HEADER)?", {
+    renderDecision,
+    shouldntBeAuthed,
+    isAuthenticated,
+    loginStatus,
+    token,
+    referer,
+    getQuery,
+    hocLoginState
+  });
+  if (renderDecision === "okay to render") {
+    return (
+      <Flex
+        flexDirection="column"
+        width={breakWidths}
+        sx={{ borderBottom: "2px #eee solid" }}
+      >
+        <Nav minHeight="58px" width={breakWidths} px={3}>
+          <MyLink shade="dark" href="/" name="home">
+            Home
+          </MyLink>
+          <RenderLogout syncLogout={syncLogout} hocLogout={hocLogout} />
+          <RenderAvatarLink />
+          {/* <MyLink shade="dark" href="/hello" name="/hello">
+            Hello
+          </MyLink>
+          <MyLink shade="dark" href="/user/profile" name="/user/profile">
+            Profile
+          </MyLink> */}
+          {/* <Label label={`counter: ${count}`} /> */}
+        </Nav>
+        {/* <button type="button" onClick={increment}>
+          increment
+        </button> */}
+        <Logger label={`counter: ${count}`} />
+      </Flex>
+    );
+  }
+  if (renderDecision === "do not render") {
+    return (
+      <Flex
+        flexDirection="column"
+        width={breakWidths}
+        sx={{ borderBottom: "2px #eee solid" }}
+      >
+        <Nav minHeight="58px" width={breakWidths} px={3}>
+          <MyLink shade="dark" href="/" name="home">
+            Home
+          </MyLink>
+
+          <RenderLoginLink />
+          {/* 
+          <RenderLogout syncLogout={syncLogout} hocLogout={hocLogout} />
+          <MyLink shade="dark" href="/hello" name="/hello">
+            Hello
+          </MyLink>
+          <MyLink shade="dark" href="/user/profile" name="/user/profile">
+            Profile
+          </MyLink> */}
+          {/* <Label label={`counter: ${count}`} /> */}
+        </Nav>
+        {/* <button type="button" onClick={increment}>
+          increment
+        </button> */}
+        <Logger label={`counter: ${count}`} />
+      </Flex>
+    );
+  } else {
+    return <div>Error!!!!!</div>;
+  }
+  // HIDE BELOW
+  // return (
+  //   <Flex
+  //     flexDirection="column"
+  //     width={breakWidths}
+  //     sx={{ borderBottom: "2px #eee solid" }}
+  //   >
+  //     <Nav minHeight="58px" width={breakWidths} px={3}>
+  //       <MyLink shade="dark" href="/" name="home">
+  //         Home
+  //       </MyLink>
+  //       {/* {isAuthenticated ? (
+  //         <RenderLogout syncLogout={syncLogout} hocLogout={hocLogout} />
+  //       ) : (
+  //         ""
+  //       )} */}
+  //       {shouldntBeAuthed === false || isAuthenticated === true ? (
+  //         <>
+  //           shouldntBeAuthed: {shouldntBeAuthed.toString()}
+  //           isAuthed: {isAuthenticated.toString()}
+  //           <RenderLogout syncLogout={syncLogout} hocLogout={hocLogout} />
+  //           <RenderAvatarLink />
+  //         </>
+  //       ) : (
+  //         <>
+  //           shouldntBeAuthed: {shouldntBeAuthed.toString()}
+  //           isAuthed: {isAuthenticated.toString()}
+  //           <RenderLoginLink />
+  //           <RenderLogout syncLogout={syncLogout} hocLogout={hocLogout} />
+  //         </>
+  //       )}
+
+  //       <MyLink shade="dark" href="/hello" name="/hello">
+  //         Hello
+  //       </MyLink>
+
+  //       <MyLink shade="dark" href="/user/profile" name="/user/profile">
+  //         Profile
+  //       </MyLink>
+  //       <Label label={`counter: ${count}`} />
+  //     </Nav>
+  //     <button type="button" onClick={increment}>
+  //       increment
+  //     </button>
+
+  //     <Logger label={`counter: ${count}`} />
+  //   </Flex>
+  // );
 };
 
 export default Header;
