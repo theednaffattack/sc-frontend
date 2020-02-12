a7d075b3ba2bdb7509943a9196a0eb7b103f03b4
eddie naff <eddienaff@eddienaff.com>
2020-02-12 13:30:15 -0800

    Code cleanup - comments

--- modules/grid-pieces/direct-message-header.tsx
+++ modules/grid-pieces/direct-message-header.tsx
@@ -11,35 +11,16 @@ import { User } from "../gql-gen/generated/apollo-graphql";
 import AvatarPlaceholder from "../profile/avatar-placeholder";
 
 interface DirectMessageHeaderProps {
-  threadId: string;
-  // selectedDirectMessageInvitees: string;
-  // setSelectedDirectMessageInvitees: React.Dispatch<
-  //   React.SetStateAction<string>
-  // >;
+  threadId?: string;
 
   selectedDirectMessageInvitees: ({
     __typename?: "User" | undefined;
   } & Pick<User, "id" | "name">)[];
-
-  setSelectedDirectMessageInvitees: React.Dispatch<
-    React.SetStateAction<
-      ({
-        __typename?: "User" | undefined;
-      } & Pick<User, "id" | "name">)[]
-    >
-  >;
 }
-
-// type TIsStringEmpty = "string_is_empty" | "string_is_NOT_empty";
 
 const DirectMessageHeader: React.FunctionComponent<DirectMessageHeaderProps> = ({
   selectedDirectMessageInvitees
 }) => {
-  // const isStringEmpty: TIsStringEmpty =
-  //   typeof selectedDirectMessageInvitees === "string" &&
-  //   selectedDirectMessageInvitees.length > 0
-  //     ? "string_is_NOT_empty"
-  //     : "string_is_empty";
   return (
     <HeaderWrapper>
       <Flex justifyContent="center" width={1}>
@@ -52,9 +33,8 @@ const DirectMessageHeader: React.FunctionComponent<DirectMessageHeaderProps> = (
           }}
         >
           {selectedDirectMessageInvitees.map((invitee, index) => (
-            // <span key={invitee.id ?? index}>{invitee.name}</span>
             <StyledListItem
-              key={invitee.id ?? `thread-invitee-${index}`}
+              key={`thread-invitee-${invitee.id}` ?? `thread-invitee-${index}`}
               mx={2}
             >
               <Flex flexDirection="column" alignItems="center">
