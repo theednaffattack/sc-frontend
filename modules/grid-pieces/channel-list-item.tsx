ef271b7b40bc505d0ea6cd28675413da9636f3b5
eddie naff <eddienaff@eddienaff.com>
2020-02-12 13:29:02 -0800

    Remove dispatching, introduce loader

--- modules/grid-pieces/channel-list-item.tsx
+++ modules/grid-pieces/channel-list-item.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Maybe from "graphql/tsutils/Maybe";
 
 import { SidebarLink, StyledListItem } from "../primitives/styled-rebass";
+import { SC_Word as Word } from "../grid-pieces/content-placeholder";
 import { User } from "../gql-gen/generated/apollo-graphql";
 
 export interface ChannelListItemProps {
   id?: string | null | undefined;
   name: string;
-  teamId: string;
+  teamId?: string | undefined;
   highlight: boolean;
   invitees: Maybe<
     Maybe<
@@ -17,21 +18,17 @@ export interface ChannelListItemProps {
     >[]
   >;
   __typename?: "Channel" | undefined;
-  setChannelName: React.Dispatch<React.SetStateAction<string>>;
-  setOnClickValue: string;
 }
 
 export const ChannelListItem: React.FunctionComponent<ChannelListItemProps> = (
-  { id, name, highlight, setChannelName, setOnClickValue, teamId },
+  { id, name, highlight, teamId },
   index: number
 ) => {
   if (!id || !name) {
-    console.log("WAIT, WHAT IS THIS???", { id, teamId });
-
     return <div key={teamId}>error NULL detected</div>;
   } else {
     let newKey = `/view-team/${teamId}/channel/${id}`;
-    console.log("NEW KEY", newKey);
+
     return (
       <StyledListItem
         pl={2}
@@ -39,16 +36,18 @@ export const ChannelListItem: React.FunctionComponent<ChannelListItemProps> = (
         key={`unique-channel-${id}-${index}`}
         highlight={highlight}
       >
-        <SidebarLink
-          key={`anchor-unique-channel-${id}-${index}`}
-          hoverColor="white"
-          as={newKey}
-          href={`/view-team/[teamId]/channel/[channelId]`}
-          setOnClick={setChannelName}
-          setOnClickValue={setOnClickValue}
-        >
-          {`# ${name}`}
-        </SidebarLink>
+        {teamId ? (
+          <SidebarLink
+            key={`anchor-unique-channel-${id}-${index}`}
+            hoverColor="white"
+            as={newKey}
+            href={`/view-team/[teamId]/channel/[channelId]`}
+          >
+            {`# ${name}`}
+          </SidebarLink>
+        ) : (
+          <Word width={1} />
+        )}
       </StyledListItem>
     );
   }
