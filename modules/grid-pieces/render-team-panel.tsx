0e26d7f01ee8cc5820a0c0ac3d4c03942ef8cc3e
eddie naff <eddienaff@eddienaff.com>
2020-02-12 13:42:04 -0800

    Adjust highlighting

--- modules/grid-pieces/render-team-panel.tsx
+++ modules/grid-pieces/render-team-panel.tsx
@@ -11,13 +11,36 @@ import {
   useGetAllTeamsForUserQuery
 } from "../../modules/gql-gen/generated/apollo-graphql";
 import { MappedTeamsProps, TeamListItem } from "./team-list-item";
+import { noQParams } from "../site-layout/grid-layout_v3";
 
-interface I_TeamPanelInfo extends GetAllTeamsForUserQueryHookResult {}
+interface I_TeamPanelInfo extends GetAllTeamsForUserQueryHookResult {
+  selectedTeamId: string;
+}
+
+const Pre = (props: any) => (
+  <Text
+    {...props}
+    as="pre"
+    sx={{
+      fontFamily: "mono",
+      p: 2,
+      color: "secondary",
+      bg: "muted",
+      overflowX: "auto"
+    }}
+  />
+);
 
 export const RenderTeamPanelReal: React.FC<I_TeamPanelInfo> = () => {
   const { data, error, loading } = useGetAllTeamsForUserQuery();
-  if (loading) return <UnstyledList>of something</UnstyledList>;
-  if (error) return <UnstyledList>of something</UnstyledList>;
+  if (loading) return <UnstyledList>loading Team panel...</UnstyledList>;
+  if (error)
+    return (
+      <UnstyledList>
+        <Text> Error loading Team panel!:</Text>
+        <Pre>{JSON.stringify(error, null, 2)}</Pre>
+      </UnstyledList>
+    );
   const listLength = 5;
   return (
     <UnstyledList>
@@ -39,7 +62,8 @@ export const RenderTeamPanelReal: React.FC<I_TeamPanelInfo> = () => {
 export const RenderTeamPanel: React.FC<I_TeamPanelInfo> = ({
   data,
   error,
-  loading
+  loading,
+  selectedTeamId
 }) => {
   if (error)
     return (
@@ -61,13 +85,24 @@ export const RenderTeamPanel: React.FC<I_TeamPanelInfo> = ({
   if (data) {
     let mappedTeams: MappedTeamsProps[] = data.getAllTeamsForUser.map(
       (team, index) => {
-        const returnTeamObj = {
-          __typename: team.__typename,
-          highlight: index === 0 ? true : false,
-          id: team.id,
-          name: team.name,
-          channels: team.channels
-        };
+        let returnTeamObj;
+        if (selectedTeamId !== noQParams.teamId) {
+          returnTeamObj = {
+            __typename: team.__typename,
+            highlight: team.id === selectedTeamId ? true : false,
+            id: team.id,
+            name: team.name,
+            channels: team.channels
+          };
+        } else {
+          returnTeamObj = {
+            __typename: team.__typename,
+            highlight: index === 0 ? true : false,
+            id: team.id,
+            name: team.name,
+            channels: team.channels
+          };
+        }
         return returnTeamObj;
       }
     );
