30b83c238770c0fc666251eb1a8f4fa9549fa3f3
eddie naff <eddienaff@eddienaff.com>
2020-02-12 17:35:01 -0800

    Change input key from userId to email

--- modules/gql-gen/query-documents/team/mutations/add-team-member.ts
+++ modules/gql-gen/query-documents/team/mutations/add-team-member.ts
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost";
 
 export const ADD_TEAM_MEMBER = gql`
-  mutation AddTeamMember($teamId: String!, $userId: String!) {
-    addTeamMember(teamId: $teamId, userId: $userId)
+  mutation AddTeamMember($teamId: String!, $email: String!) {
+    addTeamMember(teamId: $teamId, email: $email)
   }
 `;
