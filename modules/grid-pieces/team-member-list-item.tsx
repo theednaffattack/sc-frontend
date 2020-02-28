f1d549e02ae564ed4b9aa92dd1ac0f69d3246a53
eddie naff <eddienaff@eddienaff.com>
2020-02-27 17:58:31 -0800

    Create list item for team members

---
+++ modules/grid-pieces/team-member-list-item.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+import {
+  Button,
+  Flex,
+  MaterialIconBase,
+  StyledListItem
+} from "../primitives/styled-rebass";
+import { ModalStatesType } from "../site-layout/grid-layout_v3";
+import { User } from "../gql-gen/generated/apollo-graphql";
+
+// interface UserLikeProps {
+//   id: string;
+//   name: string;
+//   // __typename: string;
+// }
+
+type UserLikeProps = Pick<User, "id" | "name">;
+
+export interface TeamMemberListItemProps {
+  // userToTeamId: string;
+  // teamId: string;
+  // user: UserLikeProps
+  __typename?: "UserToTeam";
+
+  isMe?: boolean;
+  // id?: string | null | undefined;
+  name?: string | null | undefined;
+
+  userToTeamId?: string;
+  teamId?: string;
+
+  user?: UserLikeProps;
+
+  adminEditUserModalState?: ModalStatesType;
+  setAdminEditUserModalState?: React.Dispatch<
+    React.SetStateAction<ModalStatesType>
+  >;
+}
+
+export const TeamMemberListItem: React.FunctionComponent<TeamMemberListItemProps> = (
+  {
+    user,
+    // id,
+    isMe,
+    setAdminEditUserModalState,
+    adminEditUserModalState
+  },
+  index
+) => {
+  return (
+    <StyledListItem pl={2} py={1} key={`user-${index}`}>
+      <Flex>
+        <Flex alignItems="center" mr={2}>
+          <MaterialIconBase
+            name="lens"
+            size="1em"
+            fill={isMe ? "yellow" : "#38978D"}
+          />
+        </Flex>
+        <Flex alignItems="center">{user ? user.name : "NO USER"}</Flex>
+        <Flex ml="auto" mr={2}>
+          {user && user.id !== "fake" ? (
+            <Button
+              type="button"
+              onClick={() => {
+                setAdminEditUserModalState !== undefined
+                  ? setAdminEditUserModalState(
+                      adminEditUserModalState === "isClosed"
+                        ? "isOpen"
+                        : "isClosed"
+                    )
+                  : null;
+              }}
+            >
+              Edit
+            </Button>
+          ) : (
+            ""
+          )}
+        </Flex>
+      </Flex>
+    </StyledListItem>
+  );
+};
