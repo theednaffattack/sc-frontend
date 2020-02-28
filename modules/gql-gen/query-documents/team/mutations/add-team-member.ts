5587399b863b0f243da88ea2594bb8ec88d9cba8
eddie naff <eddienaff@eddienaff.com>
2020-02-27 17:55:27 -0800

    Add roles to assign authorization

--- modules/gql-gen/query-documents/team/mutations/add-team-member.ts
+++ modules/gql-gen/query-documents/team/mutations/add-team-member.ts
@@ -1,7 +1,15 @@
 import { gql } from "apollo-boost";
 
 export const ADD_TEAM_MEMBER = gql`
-  mutation AddTeamMember($teamId: String!, $email: String!) {
-    addTeamMember(teamId: $teamId, email: $email)
+  mutation AddTeamMember(
+    $roles: [TeamRoleEnum!]!
+    $teamId: String!
+    $email: String!
+  ) {
+    addTeamMember(roles: $roles, teamId: $teamId, email: $email) {
+      userId
+      teamId
+      teamRoleAuthorizations
+    }
   }
 `;
