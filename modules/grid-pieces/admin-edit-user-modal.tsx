7f43cd1bdd594241752b69ffe53318d76a7cab30
eddie naff <eddienaff@eddienaff.com>
2020-02-27 17:57:10 -0800

    Add admin user editing component

---
+++ modules/grid-pieces/admin-edit-user-modal.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { UniversalPortal } from "@jesstelford/react-portal-universal";
+import Link from "next/link";
+
+import {
+  AbFlex,
+  Card,
+  Button,
+  Flex,
+  StyledHr,
+  Text
+} from "../primitives/styled-rebass";
+import { ModalStates } from "../site-layout/grid-layout";
+import {
+  MeQuery
+  // useAdminEditUserInfoMutation
+} from "../gql-gen/generated/apollo-graphql";
+import AvatarPlaceholder from "../profile/avatar-placeholder";
+
+interface AdminEditUserModalProps {
+  userInfo: MeQuery["me"] | undefined;
+  adminEditUserModal: string;
+  setAdminEditUserModal: React.Dispatch<React.SetStateAction<ModalStates>>;
+  teamId?: string;
+}
+
+export type MessageBoxState = "isOpen" | "isClosed";
+
+export const AdminEditUserModal: React.FunctionComponent<AdminEditUserModalProps> = ({
+  adminEditUserModal,
+  setAdminEditUserModal,
+  teamId,
+  userInfo
+}) => {
+  // let [adminEditUserMutation, {data}] = useAdminEditUserInfoMutation()
+
+  return (
+    <>
+      {adminEditUserModal === "isOpen" ? (
+        <UniversalPortal selector="#modal">
+          <AbFlex
+            position="fixed"
+            bg="rgba(0, 0, 0, 0.7)"
+            top={0}
+            width={[1 / 2, 1 / 2, 1 / 3, 1 / 5, 1 / 5]}
+            // left={150}
+            right={0}
+            bottom={0}
+            border="lime"
+          >
+            <Card p={0} pb={3} width={1}>
+              <Flex
+                p={2}
+                mb={2}
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+                style={{ position: "relative" }}
+              >
+                <Text>ADMIN Edit User</Text>
+              </Flex>
+              <StyledHr my={0} />
+              <Flex
+                px={2}
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center"
+              >
+                <AvatarPlaceholder />
+                <Flex my={3} flexDirection="column">
+                  <Text>{userInfo ? userInfo.name : ""}</Text>
+                  <Text>{userInfo?.email}</Text>
+                </Flex>
+                <Flex my={3} flexDirection="column">
+                  <Text>TEAM ID</Text>
+
+                  {teamId ? teamId : "no team ID"}
+                </Flex>
+                {/* <Button type="button" onClick={()=>}>Logout</Button> */}
+                <Link href="/logout" passHref={true}>
+                  <a>logout</a>
+                </Link>
+
+                <Button
+                  type="button"
+                  onClick={() => setAdminEditUserModal("isClosed")}
+                >
+                  Close
+                </Button>
+              </Flex>
+            </Card>
+          </AbFlex>
+        </UniversalPortal>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
